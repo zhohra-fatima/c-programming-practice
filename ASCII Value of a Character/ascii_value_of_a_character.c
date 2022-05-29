f643b702f9d182eb2cb4833c1a1822a1967c1ef4
Fatima Tuz Zhohra <fatima.tuz.zhohra@gmail.com>
2022-05-29 21:14:29 +0600

    ASCII-value-of-a-character

---
+++ ASCII Value of a Character/ascii_value_of_a_character.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+
+    char ch;
+
+    printf("Enter a Character : ");
+
+    scanf("%c", &ch);
+
+    printf("\nASCII value of %c = %d \n", ch, ch);
+
+    return 0;
+
+}
